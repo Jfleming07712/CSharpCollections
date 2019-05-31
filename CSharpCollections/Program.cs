ea5a2fe29e98a530e6f6d31f18b4972e0c0ecd0d
Jfleming <Jfleming07712@gmail.com>
2019-05-31 13:39:55 -0400

    Hello Universe

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Universe!");
         }
     }
 }
